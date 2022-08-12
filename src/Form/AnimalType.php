c53f073f049377f934410a143d3714e160b4f4cd
Nathan <nathanbadoithb@gmail.com>
2022-08-12 10:34:31 +0200

    Update button backAdmin

--- src/Form/AnimalType.php
+++ src/Form/AnimalType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Animal;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,6 +16,14 @@ class AnimalType extends AbstractType
             ->add('species', null, [
                 'label' => 'Espèce',
             ])
+            //ajouter un bouton de type submit
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => [
+                    'class' => 'btn btn-primary',
+                ],
+            ])
+            
             // ->add('products')
         ;
     }
